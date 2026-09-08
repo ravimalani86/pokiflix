fac14f1ddbb02e314c9bcf8422956267a14e51db
Ravi Malani <ravi.malani86@gmail.com>
2026-09-08 18:20:26 +0530

    Revert "Improve website"
    This reverts commit 055a74c11bbe72a8060a64c22bae8f3d78754b87.

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php
-/** API credentials for the TMDB-backed catalog service. Server-side only - never echoed to the browser. */
-define('API_BASE_URL', 'http://app.myappworld.in/tmdb/api/');
-define('API_KEY', 'f3a9d6c1b8e5f27a4d0c9e6b1a7f5d82');
